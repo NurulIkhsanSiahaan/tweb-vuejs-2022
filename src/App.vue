43a73ab73f9572e4e343c27fd9c7e8805e62f4fa
unknown <11850114516@students.uin-suska.ac.id>
2022-10-08 23:33:36 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <header>
+    <img alt="Profile" class="photo" src="./assets/Profile.JPG.jpg" width="236" height="355" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Nurul Ikhsan S, S.T (Soon)" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.photo {
+  display: block;
+  margin: 0 auto 2rem;
+  border-radius: 100%;
+  border: 10px solid #fff;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .photo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,80 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import ToolingIcon from "./icons/IconTooling.vue";
+import EcosystemIcon from "./icons/IconEcosystem.vue";
+</script>
+
+<template class="body">
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Biodata</template>
+
+    <ul>
+      <p>Nama : Nurul Ikhsan Siahaan Al</p>
+      <p>NIM : 11850114516</p>
+      <p>Prodi : Teknik Informatika</p>
+      <p>Fakultas : Sains dan Tenologi</p>
+    </ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Hobi</template>
+
+    <ul>
+      <p>Hobi saya adalah bermain Badminton.</p>
+    </ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Riwayat Pendidikan </template>
+
+    <ul>
+      <li>
+        <h3 class="font">SD</h3>
+      </li>
+      <p>Sekolah : SDS Bina Siswa</p>
+      <p>Alamat : Di Pondok Palm Agung, Kep. Balam Jaya, Kec. Balai Jaya, Kab. Rokan Hilir</p>
+    </ul>
+    <br />
+
+    <ul>
+      <li>
+        <h3 class="font">SMP</h3>
+      </li>
+      <p>Sekolah : Madrasah Tsanawiyah (MTs) Al-Husna</p>
+      <p>Alamat : Di Dusun Salak, Kel. Bagan Sinembah, Kec. Bagan Sinembah Raya, Kab. Rokan Hilir</p>
+    </ul>
+    <br />
+
+    <ul>
+      <li>
+        <h3 class="font">SMA</h3>
+      </li>
+      <p>Sekolah : Madrasah Aliyah (MA) Al-Husna</p>
+      <p>Alamat : Di Dusun Salak, Kel. Bagan Sinembah, Kec. Bagan Sinembah Raya, Kab. Rokan Hilir</p>
+    </ul>
+    <br />
+  </WelcomeItem>
+</template>
+
+<style>
+.font {
+  font-weight: bold;
+  color: #fff;
+  margin-left: 13px;
+}
+
+p {
+  font-size: large;
+  margin-left: 25px;
+}
+</style>
